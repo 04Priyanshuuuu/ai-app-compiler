ad2bcea23edffcc3c1593abed99e63233cccdf04
Priyanshu <04priyanshuuuu@gmail.com>
2026-05-29 20:15:41 +0530

    Fix CORS

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ from pipeline.runtime_simulator import simulate_database
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
